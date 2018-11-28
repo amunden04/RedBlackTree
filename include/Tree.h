c00f043278958463543e2f21838de591668a7b63
amunden04 <amunden04@gmail.com>
2018-11-27 23:19:26 -0700

    adding changes

--- include/Tree.h
+++ include/Tree.h
@@ -1,16 +1,27 @@
 #ifndef TREE_H
 #define TREE_H
 
+enum colour {RED, BLACK, DOUBLEBLACK};
+
+struct {
+    int data;
+    int colour;
+    Node *left, *right, *parent;
+};
 
 class Tree
 {
     public:
         Tree();
         virtual ~Tree();
+        void insertValue(int);
+        void deleteValue(int);
+        void findNode();
 
     protected:
 
     private:
+        Node *root;
 };
 
 #endif // TREE_H
--- src/Tree.cpp
+++ src/Tree.cpp
@@ -1,2 +1,14 @@
 #include "Tree.h"
+#include <iostream>
 
+using namespace std;
+
+Node::Node(int data) {
+    this->data = data;
+    colour = RED;
+    left = right = parent = nullptr;
+}
+
+Tree::Tree() {
+    root = nullptr;
+}
